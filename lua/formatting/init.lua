463e1fe990d6ec9d61e2c510af27e4ca66e73012
ddvuser <denisdoroshenko99@gmail.com>
2023-12-03 18:14:36 +0200

    change file name

--- lua/formatting/init.lua
+++ lua/formatting/init.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function()
 
-  local autoformat_setup = require('formatting.autoformat-setup')
+  local autoformat_setup = require('formatting.autoformat')
   autoformat_setup.setup()
 
   local cmp_setup = require('formatting.cmp-setup')
